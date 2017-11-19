97bbffda232ae5d8b812f17b846117f275192ed9
yuemori <yuemori@aiming-inc.com>
2017-11-20 01:49:57 +0900

    Add watch resource

--- kubeterm/client.go
+++ kubeterm/client.go
@@ -4,6 +4,7 @@ import (
 	"github.com/yuemori/kubeterm/kubernetes"
 	v1core "k8s.io/client-go/kubernetes/typed/core/v1"
 	v1 "k8s.io/client-go/pkg/api/v1"
+	"k8s.io/client-go/pkg/watch"
 )
 
 type Client struct {
@@ -28,5 +29,33 @@ func NewClient(config *Config) *Client {
 		Namespaces: nss,
 	}
 
+	c.watchNamespace()
+
 	return c
+}
+
+func (c *Client) watchNamespace() error {
+	watcher, err := c.client.Namespaces().Watch(v1.ListOptions{Watch: true})
+
+	if err != nil {
+		return err
+	}
+
+	go func() {
+		for {
+			select {
+			case e := <-watcher.ResultChan():
+				switch e.Type {
+				case watch.Added:
+					nss, _ := c.client.Namespaces().List(v1.ListOptions{})
+					c.Namespaces = nss
+				case watch.Deleted:
+					nss, _ := c.client.Namespaces().List(v1.ListOptions{})
+					c.Namespaces = nss
+				}
+			}
+		}
+	}()
+
+	return nil
 }
